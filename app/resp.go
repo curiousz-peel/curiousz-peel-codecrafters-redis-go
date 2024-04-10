7749034ce4962d4ea10785e040f31e0a8dd16e89
Ito <ito@Cristinas-MacBook-Air.local>
2024-04-11 01:10:04 +0300

    passed 5th stage
    test regex exp
    
    test regex exp from gpt
    
    passed 5th stage
    
    pass 5th stage
    
    debug log
    
    more debug logs
    
    even more debug logs
    
    fix  debug logs
    
    removed constants
    
    improved logs
    
    log command split using regex
    
    print data
    
    try to fix regex exp
    
    added unescaped logs
    
    added validations
    
    try to output raw resp string
    
    removed escaped special characters from regex statement
    
    found bug
    
    handle EOF error
    
    fixed special character escape
    
    reintroduced constants, removed logs
    
    corrected constants
    
    reverted to raw regex expressions
    
    retry pipeline
    
    final stage 5 commit
    
    pass 5th stage

---
+++ app/resp.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"regexp"
+	"strings"
+)
+
+type respHandler struct {
+	data     []byte
+	commands map[string]string
+	conn     net.Conn
+}
+
+func InitRESP(conn net.Conn) *respHandler {
+	return &respHandler{conn: conn, commands: make(map[string]string)}
+}
+
+func (r *respHandler) Read() error {
+	buf := make([]byte, 128)
+	_, err := r.conn.Read(buf)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+		fmt.Printf("couldn't read from %v\n", r.conn.LocalAddr().String())
+		return err
+	}
+	r.data = buf
+	return nil
+}
+
+func (r *respHandler) Parse() {
+	var isArg bool
+	var currentCommand string
+	splitRe := regexp.MustCompile(`\$(\d+)\r\n(\w+)`)
+	trimRe := regexp.MustCompile(`\$(\d+)\r\n`)
+
+	tokens := splitRe.FindAll(r.data, -1)
+	if tokens != nil {
+		for _, token := range tokens {
+			token = trimRe.ReplaceAll(token, []byte(""))
+			if !isArg {
+				currentCommand = string(token)
+				r.commands[currentCommand] = ""
+			} else {
+				r.commands[currentCommand] = string(token)
+			}
+			isArg = true
+		}
+		return
+	}
+	fmt.Printf("no tokens after regex expression: %s\n", splitRe.String())
+}
+
+func (r respHandler) handleCommand(command string, argument string) (response []byte, err error) {
+	switch strings.ToLower(command) {
+	case "ping":
+		response = []byte("+PONG\r\n")
+	case "echo":
+		response = []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(argument), argument))
+	default:
+		err = fmt.Errorf("unknown command %s", command)
+	}
+	return
+}
+
+func (r respHandler) Execute() error {
+	if r.commands != nil {
+		for comm, arg := range r.commands {
+			resp, err := r.handleCommand(comm, arg)
+			if err != nil {
+				return err
+			}
+			_, err = r.conn.Write(resp)
+			if err != nil {
+				return fmt.Errorf("couldn't write to %s: %v", r.conn.LocalAddr().String(), err.Error())
+			}
+		}
+		return nil
+	}
+	return fmt.Errorf("no commands to execute")
+}
--- app/server.go
+++ app/server.go
@@ -7,9 +7,6 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -20,7 +17,6 @@ func main() {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
-			// continue
 			os.Exit(1)
 		}
 		go handleConnection(conn)
@@ -30,21 +26,19 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 128)
+	respHandler := InitRESP(conn)
 	for {
-		bitesRead, err := conn.Read(buf)
+		err := respHandler.Read()
 		if err != nil {
-			fmt.Printf("couldn't read from %v\n", conn.LocalAddr().String())
-			return
+			fmt.Println(err)
+			os.Exit(2)
+
 		}
-
-		fmt.Println("received data", buf[:bitesRead])
-
-		resp := []byte("+PONG\r\n")
-		_, err = conn.Write(resp)
+		respHandler.Parse()
+		err = respHandler.Execute()
 		if err != nil {
-			fmt.Printf("couldn't write to %v\n", conn.LocalAddr().String())
+			fmt.Println(err)
+			os.Exit(3)
 		}
 	}
-
 }
